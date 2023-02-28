86160a380b9b0764cb4f9cf0622aa845f16d404c
Samir Ansari <developedbysam@gmail.com>
2023-02-28 01:50:38 -0600

    completed

---
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.example.wordle
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.guessButton)
+        val wordToGuess = FourLetterWordList.getRandomFourLetterWord()
+        val textView: TextView = findViewById<TextView>(R.id.guessWord)
+
+        val hiddenWord = findViewById<TextView>(R.id.hiddenWord)
+
+        val guess1 = findViewById<TextView>(R.id.guess1)
+        val check1 = findViewById<TextView>(R.id.check1)
+
+        val guess2 = findViewById<TextView>(R.id.guess2)
+        val check2 = findViewById<TextView>(R.id.check2)
+
+        val guess3 = findViewById<TextView>(R.id.guess3)
+        val check3 = findViewById<TextView>(R.id.check3)
+
+        var count = 1
+        button.setOnClickListener (View.OnClickListener() {
+            if (count <= 3) {
+                val guess: String = textView.text.toString()
+                val output = checkGuess(guess, wordToGuess)
+
+                when (count) {
+                    1 -> {
+                        guess1.text = guess
+                        check1.text = output
+                    }
+
+                    2 -> {
+                        guess2.text = guess
+                        check2.text = output
+                    }
+
+                    3 -> {
+                        guess3.text = guess
+                        check3.text = output
+                    }
+                }
+
+                count++
+            } else {
+                hiddenWord.text = wordToGuess
+                Toast.makeText(it.context, "Number of guess Exceeded!!", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    // author: calren
+    object FourLetterWordList {
+        // List of most common 4 letter words from: https://7esl.com/4-letter-words/
+        val fourLetterWords =
+            "Area,Army,Baby,Back,Ball,Band,Bank,Base,Bill,Body,Book,Call,Card,Care,Case,Cash,City,Club,Cost,Date,Deal,Door,Duty,East,Edge,Face,Fact,Farm,Fear,File,Film,Fire,Firm,Fish,Food,Foot,Form,Fund,Game,Girl,Goal,Gold,Hair,Half,Hall,Hand,Head,Help,Hill,Home,Hope,Hour,Idea,Jack,John,Kind,King,Lack,Lady,Land,Life,Line,List,Look,Lord,Loss,Love,Mark,Mary,Mind,Miss,Move,Name,Need,News,Note,Page,Pain,Pair,Park,Part,Past,Path,Paul,Plan,Play,Post,Race,Rain,Rate,Rest,Rise,Risk,Road,Rock,Role,Room,Rule,Sale,Seat,Shop,Show,Side,Sign,Site,Size,Skin,Sort,Star,Step,Task,Team,Term,Test,Text,Time,Tour,Town,Tree,Turn,Type,Unit,User,View,Wall,Week,West,Wife,Will,Wind,Wine,Wood,Word,Work,Year,Bear,Beat,Blow,Burn,Call,Care,Cast,Come,Cook,Cope,Cost,Dare,Deal,Deny,Draw,Drop,Earn,Face,Fail,Fall,Fear,Feel,Fill,Find,Form,Gain,Give,Grow,Hang,Hate,Have,Head,Hear,Help,Hide,Hold,Hope,Hurt,Join,Jump,Keep,Kill,Know,Land,Last,Lead,Lend,Lift,Like,Link,Live,Look,Lose,Love,Make,Mark,Meet,Mind,Miss,Move,Must,Name,Need,Note,Open,Pass,Pick,Plan,Play,Pray,Pull,Push,Read,Rely,Rest,Ride,Ring,Rise,Risk,Roll,Rule,Save,Seek,Seem,Sell,Send,Shed,Show,Shut,Sign,Sing,Slip,Sort,Stay,Step,Stop,Suit,Take,Talk,Tell,Tend,Test,Turn,Vary,View,Vote,Wait,Wake,Walk,Want,Warn,Wash,Wear,Will,Wish,Work,Able,Back,Bare,Bass,Blue,Bold,Busy,Calm,Cold,Cool,Damp,Dark,Dead,Deaf,Dear,Deep,Dual,Dull,Dumb,Easy,Evil,Fair,Fast,Fine,Firm,Flat,Fond,Foul,Free,Full,Glad,Good,Grey,Grim,Half,Hard,Head,High,Holy,Huge,Just,Keen,Kind,Last,Late,Lazy,Like,Live,Lone,Long,Loud,Main,Male,Mass,Mean,Mere,Mild,Nazi,Near,Neat,Next,Nice,Okay,Only,Open,Oral,Pale,Past,Pink,Poor,Pure,Rare,Real,Rear,Rich,Rude,Safe,Same,Sick,Slim,Slow,Soft,Sole,Sore,Sure,Tall,Then,Thin,Tidy,Tiny,Tory,Ugly,Vain,Vast,Very,Vice,Warm,Wary,Weak,Wide,Wild,Wise,Zero,Ably,Afar,Anew,Away,Back,Dead,Deep,Down,Duly,Easy,Else,Even,Ever,Fair,Fast,Flat,Full,Good,Half,Hard,Here,High,Home,Idly,Just,Late,Like,Live,Long,Loud,Much,Near,Nice,Okay,Once,Only,Over,Part,Past,Real,Slow,Solo,Soon,Sure,That,Then,This,Thus,Very,When,Wide"
+
+        // Returns a list of four letter words as a list
+        fun getAllFourLetterWords(): List<String> {
+            return fourLetterWords.split(",")
+        }
+
+        // Returns a random four letter word from the list in all caps
+        fun getRandomFourLetterWord(): String {
+            val allWords = getAllFourLetterWords()
+            val randomNumber = (0..allWords.size).shuffled().last()
+            return allWords[randomNumber].uppercase()
+        }
+    }
+
+    /**
+     * Parameters / Fields:
+     *   wordToGuess : String - the target word the user is trying to guess
+     *   guess : String - what the user entered as their guess
+     *
+     * Returns a String of 'O', '+', and 'X', where:
+     *   'O' represents the right letter in the right place
+     *   '+' represents the right letter in the wrong place
+     *   'X' represents a letter not in the target word
+     */
+    private fun checkGuess(guess: String, wordToGuess: String) : String {
+        var result = ""
+        for (i in 0..3) {
+            if (guess[i] == wordToGuess[i]) {
+                result += "O"
+            }
+            else if (guess[i] in wordToGuess) {
+                result += "+"
+            }
+            else {
+                result += "X"
+            }
+        }
+        return result
+    }
+}
